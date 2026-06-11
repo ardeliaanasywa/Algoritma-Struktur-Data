43093d4d6d211b96934a35321cb97d3db69c3adc
ardeliaanasywa <ardeliaanasywa@gmail.com>
2026-06-11 22:12:04 +0800

    Add files via upload

---
+++ modul7algo/soal.cpp
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+struct Node
+{
+    int data;
+    Node *kiri;
+    Node *kanan;
+};
+
+void tambah(Node **root, int databaru)
+{
+    if (*root == NULL)
+    {
+        Node *baru;
+        baru = new Node;
+
+        baru->data = databaru;
+        baru->kiri = NULL;
+        baru->kanan = NULL;
+        (*root) = baru;
+        (*root)->kiri = NULL;
+        (*root)->kanan = NULL;
+        cout << "Data Bertambah";
+    }
+    else if (databaru < (*root)->data)
+        tambah(&((*root)->kiri), databaru);
+    else if (databaru > (*root)->data)
+        tambah(&((*root)->kanan), databaru);
+    else if (databaru == (*root)->data)
+        cout << "\nData sudah ada";
+}
+
+void preOrder(Node *root)
+{
+    if (root != NULL)
+    {
+        cout << root->data << " ";
+        preOrder(root->kiri);
+        preOrder(root->kanan);
+    }
+}
+
+void inOrder(Node *root)
+{
+    if (root != NULL)
+    {
+        inOrder(root->kiri);
+        cout << root->data << " ";
+        inOrder(root->kanan);
+    }
+}
+
+void postOrder(Node *root)
+{
+    if (root != NULL)
+    {
+        postOrder(root->kiri);
+        postOrder(root->kanan);
+        cout << root->data << " ";
+    }
+}
+
+int main()
+{
+    int pil, data;
+    Node *pohon;
+    pohon = NULL;
+    do
+    {
+        system("cls");
+        cout << "====================================\n";
+        cout << "   PROGRAM BINARY SEARCH TREE (BST)\n";
+        cout << "====================================\n";
+        cout << "1. Tambah\n";
+        cout << "2. PreOrder\n";
+        cout << "3. InOrder\n";
+        cout << "4. PostOrder\n";
+        cout << "5. Exit\n";
+        cout << "====================================\n";
+        cout << "Pilihan : ";
+        cin >> pil;
+        switch (pil)
+        {
+        case 1:
+            cout << "\nINPUT : ";
+            cout << "\n---------------------";
+            cout << "\nData Baru : ";
+            cin >> data;
+            tambah(&pohon, data);
+            break;
+        case 2:
+            cout << "\nPreOrder";
+            cout << "\n---------------------\n";
+            if (pohon != NULL)
+            {
+                preOrder(pohon);
+            }
+            else
+                cout << "Masih Kosong";
+            break;
+        case 3:
+            cout << "\nInOrder";
+            cout << "\n---------------------\n";
+            if (pohon != NULL)
+            {
+                inOrder(pohon);
+            }
+            else
+                cout << "Masih Kosong";
+            break;
+        case 4:
+            cout << "\nPostOrder";
+            cout << "\n---------------------\n";
+            if (pohon != NULL)
+            {
+                postOrder(pohon);
+            }
+            else
+                cout << "Masih Kosong";
+            break;
+        case 5:
+            cout << "\nProgram Selesai...";
+            return 0;
+
+        default:
+            cout << "\nPilihan tidak tersedia!";
+        }
+
+        cout << "\n\nTekan sembarang tombol untuk kembali ke menu...";
+        _getch();
+
+    } while (pil != 5);
+    return EXIT_FAILURE;
+}
